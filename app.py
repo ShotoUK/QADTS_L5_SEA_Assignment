cc63b412572d917920fb3893b58ddaf0c9f4b276
Charlton Reid <shoto_uk@outlook.com>
2024-01-24 09:27:52 +0000

    Error controllers and templates

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello {}!</h1>'.format(name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
