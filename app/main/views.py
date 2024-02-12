1501186a6699a2961799cb9fd2fee785b9074a69
Charlton Reid <shoto_uk@outlook.com>
2024-02-12 23:00:43 +0000

    Add customer detail page and display customer list on the index page

--- app/main/views.py
+++ app/main/views.py
@@ -2,20 +2,28 @@ from flask import Flask, request, redirect, abort, render_template
 from flask_login import login_required
 from app.main.forms import NameForm, LoginForm, CreateUserForm
 from .. import db
-from app.models import User
+from app.models import User, Customer
 from . import main
 
 @main.route('/', methods=['GET','POST'])
 @login_required
 def index():
     name = None
+    customers = Customer.query.all()
     form = NameForm()
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+    return render_template('index.html', form=form, name=name, customers=customers)
+
+@main.route('/customer/<int:id>', methods=['GET','POST'])
+@login_required
+def customerdetail(id):
+    customer = Customer.query.get_or_404(id)
+    return render_template('customer.html', customer=customer)
   
 @main.route('/user/create', methods=['GET','POST'])
+@login_required
 def create_user():
     form = CreateUserForm()
     if request.method == 'POST':
