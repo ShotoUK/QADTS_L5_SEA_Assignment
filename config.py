240010550139bd2d6b97eb00dae04eec04c28d7b
Charlton Reid <shoto_uk@outlook.com>
2024-03-13 11:08:58 +0000

    added Heroku config to dictionary.

--- config.py
+++ config.py
@@ -42,5 +42,6 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig,
     'default': DevelopmentConfig
 }
