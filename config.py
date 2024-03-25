ce9940373716b2bd0114165c72abda4ff8956838
Charlton Reid <shoto_uk@outlook.com>
2024-03-25 23:41:05 +0000

    Update config.py and removed hard coded credentials

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'WkmpPFr+Sw9Nz5:6EM3;Zq'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
     SQLALCHEMY_BINDS = {'default':'sqlite:///' + os.path.join(basedir,'data.sqlite')}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SSL_REDIRECT = False
-    MAIL_SERVER = 'us2.smtp.mailhostbox.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'charlton@ballincoder.tech'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '@^tHMUS#N4'
-    FLASKY_MAIL_SUBJECT_PREFIX = '[TigersolvCRM]'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_SERVER')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Tigersolv]'
     FLASKY_MAIL_SENDER = 'Tigersolv CRM <charlton@ballincoder.tech>'
 
     @staticmethod
