9abe0dd0f65271954c7770e8d69f4c1ee59bb66a
Charlton Reid <shoto_uk@outlook.com>
2024-03-27 17:42:36 +0000

    Update MAIL_USE_TLS and MAIL_USE_SSL settings

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     SSL_REDIRECT = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     CRM_MAIL_SUBJECT_PREFIX = os.environ.get('CRM_MAIL_SUBJECT_PREFIX')
