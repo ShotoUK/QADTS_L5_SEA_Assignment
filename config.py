a7ab59d2bf1dff0f81fce5fe53c43861922d839b
Charlton Reid <shoto_uk@outlook.com>
2024-03-27 12:13:32 +0000

    Fix MAIL_PORT environment variable in config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SSL_REDIRECT = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
