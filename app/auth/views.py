03d6a12de7d27dc4338e61cf05474acc4023afdc
Charlton Reid <shoto_uk@outlook.com>
2024-03-27 12:11:28 +0000

    Update newUser.Confirmed value to False and comment out send_email function call

--- app/auth/views.py
+++ app/auth/views.py
@@ -75,7 +75,7 @@ def register():
                 # Set email and password
                 newUser.Email = form.email.data
                 newUser.Role = 1
-                newUser.Confirmed = True
+                newUser.Confirmed = False
 
                 # Add to database
                 db.session.add(newUser)
@@ -83,7 +83,7 @@ def register():
 
                 # Send confirmation email
                 token = newUser.generate_confirmation_token()
-                send_email(newUser.Email,'Confirm Your Account','auth/email/confirm',user=newUser,token=token)
+                # send_email(newUser.Email,'Confirm Your Account','auth/email/confirm',user=newUser,token=token)
 
                 flash('You can now login.')
 
